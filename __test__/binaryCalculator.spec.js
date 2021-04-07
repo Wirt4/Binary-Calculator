510da32974b75bc5ae47e2eb981ea72c7c70420e
Wirt4 <wirt.salthouse@gmail.com>
2021-04-06 19:59:43 -0700

    putting ones and zeroes

--- __test__/binaryCalculator.spec.js
+++ __test__/binaryCalculator.spec.js
@@ -1,13 +1,58 @@
 //remember need to import {function header} from "./scriptsrc";
-import {message} from "../js/binaryCalculator";
-
+import {Calculator} from "../js/binaryCalculator";
+//import {init} from "../js/binaryCalculator";
 describe("when message is called, the screen reads 'hello' ", () => {
-  test("pressing a button", () => {
+  test("altering screen", () => {
+    const calc = new Calculator('res');
     document.body.innerHTML = " <div id='res'></div>";
     const scrn = document.getElementById('res');
     expect(scrn.innerHTML).toBe('');
-    message();
+    calc.message();
     expect(scrn.innerHTML).toBe('hello');
+  });
+});
 
+describe("testing clear method, then clear method called by button press ", () => {
+  test("clear method", () => {
+    const calc = new Calculator();
+    document.body.innerHTML = " <div id='res'></div><div>";
+    const scrn = document.getElementById('res');
+    expect(scrn.innerHTML).toBe('');
+    calc.clear();
+    expect(scrn.innerHTML).toBe('');
+    scrn.innerHTML='101010';
+    expect(scrn.innerHTML).toBe('101010');
+    calc.clear();
+    expect(scrn.innerHTML).toBe('');
+  });
+});
+
+describe("digit entry", () => {
+  test("putting one", () => {
+    const calc = new Calculator();
+    document.body.innerHTML = " <div id='res'></div><div>";
+    const scrn = document.getElementById('res');
+    calc.putOne();
+    expect(scrn.innerHTML).toBe('1');
+  });
+  test("putting multiple ones", () => {
+    const calc = new Calculator();
+    document.body.innerHTML = " <div id='res'></div><div>";
+    const scrn = document.getElementById('res');
+    let i;
+    for (i=0; i<8; i++){
+      calc.putOne();
+    }
+    expect(scrn.innerHTML).toBe('11111111');
+    calc.clear();
+    expect(scrn.innerHTML).toBe('');
+  });
+
+  test("putting zero", () => {
+    const calc = new Calculator();
+    document.body.innerHTML = " <div id='res'></div><div>";
+    const scrn = document.getElementById('res');
+    calc.putZero();
+    expect(scrn.innerHTML).toBe('0');
   });
 });
