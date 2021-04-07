cba0d7cd1aacb26dbe334817a53ac5b59f686fe7
Wirt4 <wirt.salthouse@gmail.com>
2021-04-06 18:18:09 -0700

    restarting, testing DOM modifiers first

--- __test__/binaryCalculator.spec.js
+++ __test__/binaryCalculator.spec.js
@@ -1,93 +1,13 @@
 //remember need to import {function header} from "./scriptsrc";
-import {decimalToBinary, binaryToDecimal, bAdd, bSub} from "../js/binaryCalculator";
-import {bMul} from "../js/binaryCalculator";
-describe("testing decimal to binary", () => {
-  test("endow binary number object with 3, expect to get 11", () => {
-    expect(decimalToBinary(3)).toEqual(11);
-  });
-  test("endow binary number object with 2, expect to get 10", () => {
-    expect(decimalToBinary(2)).toEqual(10);
-  });
-  test("zero case", () => {
-    expect(decimalToBinary(0)).toEqual(0);
-  });
+import {message} from "../js/binaryCalculator";
 
-  test("negative input", () => {
-    expect(decimalToBinary(-90)).toEqual(-1);
-  });
-  test("fractions", () => {
-    expect(decimalToBinary(Math.PI)).toEqual(-1);
-  });
+describe("when message is called, the screen reads 'hello' ", () => {
+  test("pressing a button", () => {
+    document.body.innerHTML = " <div id='res'></div>";
+    const scrn = document.getElementById('res');
+    expect(scrn.innerHTML).toBe('');
+    message();
+    expect(scrn.innerHTML).toBe('hello');
 
-});
-
-describe("testing binary to decimal", () => {
-  test("basic conversion from binary to decimal", () => {
-    expect(binaryToDecimal(111)).toEqual(7);
-    expect(binaryToDecimal('011')).toEqual(3);
-  });
-  test("bad inputs", () => {
-    expect(binaryToDecimal(42)).toEqual(-1);
-    expect(binaryToDecimal(Math.PI)).toEqual(-1);
-    expect(binaryToDecimal(-1110)).toEqual(-1);
-  });
-});
-
-describe("testing addition", () => {
-  test("basic addition case", () => {
-    expect(bAdd(1, 1)).toEqual(10);
-  });
-
-  test("2 8s", () => {
-    expect(bAdd(100, 100)).toEqual(1000);
-  });
-
-  test("slightly more complex", () => {
-    expect(bAdd(1010100, 1101)).toEqual(1100001);
-  });
-
-});
-
-describe("testing subtraction", () => {
-  test("subtraction from itself", () => {
-    expect(bSub(1101, 1101)).toEqual(0);
-  });
-
-  test("a mildly complicated subtraction", () => {
-    expect(bSub(1101000, 1101)).toEqual(1011011);
-  });
-
-  test("too large subtraction, output is printed, so graceful fail", () => {
-    expect(bSub(1101, 1101000)).toEqual(0);
-  });
-});
-
-describe("testing multiplication", () => {
-  test("3x3 == 9", () => {
-    expect(bMul(11, 11)).toEqual(1001);
-  });
-
-  test("0x42 == 0", () => {
-    expect(bMul(110010, 0)).toEqual(0);
-  });
-
-  test("42x54 == 2268", () => {
-    expect(bMul(101010, 110110)).toEqual(100011011100);
-  });
-  test("1x42 == 42", () => {
-    expect(bMul(1, 110010)).toEqual(110010);
-  });
-
-});
-
-describe("Add value to field", () => {
-  test("put binary value of 47 on screen when 1 is clicked", () => {
-    document.body.innerHTML = `<div id = 'res' class='resClass'></div> <div id = 'btns'>
-    <button id='btn1' class='digitClass'>1</button></div>`;
-    const fortySeven = "101111";
-    const one= document.getElementById('btn1');
-    const screen = document.getElementById('res');
-    one.click();
-    expect(screen.innerHTML).toBe(fortySeven);
   });
 });
--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -1,85 +1,26 @@
 "use strict";
-//document.addEventListener('DOMContentLoaded', init, false);
-//function init() {
-  document.addEventListener('DOMContentLoaded', init, false);
-function init (){
-  const one = document.getElementById('btn1');
-  const zero = document.getElementById('btn0');
-  const c = document.getElementById('btnClr');
-  const eq = document.getElementById('btnEql');
-  const plus = document.getElementById('btnSum');
-  const minus = document.getElementById('btnSub');
-  const times = document.getElementById('btnMul');
-  const div = document.getElementById('btnDiv');
+document.addEventListener('DOMContentLoaded', init, false);
+function init(){
+  //var calc = new Calculator();
+  var one = document.getElementById('btn1');
+  var zero = document.getElementById('btn0');
+  var c = document.getElementById('btnClr');
+  var eq = document.getElementById('btnEql');
+  var plus = document.getElementById('btnSum');
+  var minus = document.getElementById('btnSub');
+  var times = document.getElementById('btnMul');
+  var div = document.getElementById('btnDiv');
 
-  one.addEventListener('click', () => setField());
-  zero.addEventListener('click', () => setField());
-  c.addEventListener('click', () => clearField());
-  eq.addEventListener('click', () => setField());
-  plus.addEventListener('click', () => csetField());
-  minus.addEventListener('click', () => setField());
-  times.addEventListener('click', () => setField());
-  div.addEventListener('click', () => setField());
+  one.addEventListener('click', message, true);
+  zero.addEventListener('click', message, true);
+  c.addEventListener('click', message, true);
+  eq.addEventListener('click', message, true);
+  plus.addEventListener('click', message, true);
+  minus.addEventListener('click', message, true);
+  times.addEventListener('click', message, true);
+  div.addEventListener('click', message, true);
 };
-//};
 
-//assumes both a and b are binary numbers
-
-export function bAdd(a, b){
-  const decA = binaryToDecimal(a);
-  const decB = binaryToDecimal(b);
-  return decimalToBinary(decA + decB);
-}
-
-
-export function decimalToBinary(dec) {
-  if (dec >= 0 && Math.floor(dec) == dec) {
-    let rep = "";
-    while (dec > 0) {
-      rep = dec % 2 + rep;
-      dec = Math.floor(dec / 2);
-    }
-    return Number(rep);
-  }
-  return -1;
-}
-
-export function binaryToDecimal(bin) {
-  bin = Number(bin); //to remove leading zeroes
-  if (bin >= 0 && Math.floor(bin) == bin) {
-    let res = 0;
-    let pow = 0;
-    while (bin > 0) {
-      let place = bin % 10;
-      if (place>2) return -1;
-      bin = Math.floor(bin / 10);
-      res += place * 2 ** pow;
-      pow++;
-    }
-    return res;
-  }
-  return -1;
-}
-//subtracts b from a
-export function bSub(a, b){
-  if (a>b){
-    const decA = binaryToDecimal(a);
-    const decB = binaryToDecimal(b);
-    return decimalToBinary(decA - decB);
-  }
-  return 0;
-}
-
-function setField() {
-  document.getElementById('res').innerHTML = "Greetings Program";
-}
-
-function clearField() {
-  document.getElementById('res').innerHTML = "";
-}
-
-export function bMul(a, b){
-  const decA = binaryToDecimal(a);
-  const decB = binaryToDecimal(b);
-  return decimalToBinary(decA * decB);
-}
+export function message () {
+  document.getElementById('res').innerHTML = "hello";
+};
